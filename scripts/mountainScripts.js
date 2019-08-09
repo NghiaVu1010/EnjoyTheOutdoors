ba936d1d9135ecea462b83c4ad54c766f02f1e76
Nghia Vu <Nghia.vu1010@gmail.com>
2019-08-09 13:44:37 -0400

    Finished Parks

--- scripts/mountainScripts.js
+++ scripts/mountainScripts.js
@@ -1,13 +1,26 @@
 /*
-* Description: 
+* Description: Search and populate table based on JSON file and selection
 *
 * Author: Neo
 */
 "use strict";
 
-//insertion function for table
+/*
+* Insertion function for table based on data
+* 
+* @param row (Object) - Inserts row after every data object
+* @param cell1 (Object) - Populate specified cell with data
+* @param cell2 (Object) - Populate specified cell with data
+* @param cell3 (Object) - Populate specified cell with data
+* @param cell4 (Object) - Populate specified cell with data
+* @param cell5 (Object) - Populate specified cell with data
+* @param cell6 (Object) - Populate specified cell with data
+*/
 function insertTableData(list, dataBody) {
         let row = dataBody.insertRow(-1);
+        let img = document.createElement("img");
+        img.src = list.img;
+        img.alt = list.img;
 
         let cell1 = row.insertCell(0);
         cell1.innerHTML = list.name;
@@ -19,7 +32,7 @@ function insertTableData(list, dataBody) {
         cell3.innerHTML = list.effort;
 
         let cell4 = row.insertCell(3);
-        cell4.innerHTML = list.img;
+        cell4.innerHTML = img;
 
         let cell5 = row.insertCell(4);
         cell5.innerHTML = list.desc;
@@ -28,15 +41,11 @@ function insertTableData(list, dataBody) {
         cell6.innerHTML = "lat: " + list.coords.lat + "\nlng: " + list.coords.lng;
 }
 
-function appendColumn() {
-    var tbl = document.getElementById("tours");
-
-    // open loop for each row and append cell
-    for(let i = 0; i < tbl.rows.length; i++) {
-        createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length), i, 'col');
-    }
-}
-
+/*
+* Display the results that matched the selection
+* 
+* @param dataBody (Object) - Grab the field of the ID
+*/
 function displayResults(selection, list) {
     let dataBody = document.getElementById("dataBody");
 
@@ -47,14 +56,28 @@ function displayResults(selection, list) {
     }
 }
 
+/*
+* Reset the table and replace
+* 
+* @param dataBody (Object) - Grab the field of the ID
+* @param newBody (Object) - Create a new element to replace
+*/
+function resetTable(table) {
+    let dataBody = document.getElementById("dataBody");
+    let newBody = document.createElement("tbody");
+    newBody.setAttribute("id", "dataBody");
+
+    table.replaceChild(newBody, dataBody);
+}
+
 window.onload = function() 
 {
-    let mountainField = document.getElementById("mountainField");
-
+    //grab mountains data
     let objs;
     $.getJSON("data/mountains.json", function(data) {
         objs = data.mountains;
 
+        //build ddl based on mountain data
         for(let i = 0; i < objs.length; i++) {
             let element = document.createElement("option");
             element.text = objs[i].name;
@@ -63,20 +86,22 @@ window.onload = function()
         }
     });
 
-    //grab each of the values and assigns value
+    //grab each of the fields and assign
+    let mountainField = document.getElementById("mountainField");
     let table = document.getElementById("tours");
 
+    //search mountain info
     const mountainSearchBtn = document.getElementById("mountainSearchBtn");
     mountainSearchBtn.onclick = function() {
+        resetTable(table);
+        table.style.display = 'initial';
         displayResults(mountainField.value, objs);
     };
 
     var resetBtn = document.getElementById('resetBtn');
     // Bind Click Event Handler to Reset Buttom
     resetBtn.onclick = function() {
-        let newBody = document.createElement("tbody");
-        newBody.setAttribute("id", "dataBody");
-
-        table.replaceChild(newBody, dataBody);
+        table.style.display = 'none';
+        resetTable(table);
     };
 };
